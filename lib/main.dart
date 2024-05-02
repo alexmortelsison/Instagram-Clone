fde87c9b66a6beba43ae3777a74b7cc30ed555fc
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-03 08:31:25 +0900

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyBhZIiULP4RE3WLnO19jTVwnhQes6Uca4A',
+      appId: "1:359376966581:web:6147000fb7c469bb463ffc",
+      messagingSenderId: "359376966581",
+      projectId: "instagram-clone-60793",
+      storageBucket: "instagram-clone-60793.appspot.com",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
